9caf8571d24936f7333ade9d8dad75eb43f0ee1c
jmLecias <104704307+jmLecias@users.noreply.github.com>
2024-02-11 15:49:06 +0800

    initial spa code

--- src/App.js
+++ src/App.js
@@ -1,36 +1,18 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Login from './Login';
+import Register from './Register';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className='text-center'>THREEL</h1>
-      <div className="registerContainer mx-auto my-auto">
-        <h2 className='text-center'>REGISTER</h2>
-          <form className='form-group'>
-            <input type="text" id="username" name="username" placeholder="Username" required/>
-
-            <input type="email" id="email" name="email" placeholder="Email" required/>
-
-            <input type="password" id="password" name="password" placeholder="Password" required/>
-
-            <input type="password" id="confirmPassword" name="confirmPassword" placeholder="Confirm Password" required/>
-
-            <select id="accountType" name="accountType" placeholder="Account Type">
-              <option value="" disabled defaultValue>Account Type</option>
-              <option value="listener">Listener</option>
-              <option value="artist">Artist</option>
-            </select>
-
-            <p className='mx-auto'>Already have an Account? <a href="#">Login Now!</a></p>
-
-            <div className='mx-auto'>
-              <button type="submit" className="registerButton">Register</button>
-              <button type="button" className="cancelButton">Cancel</button>
-            </div>
-          </form>
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Register.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import threel_api from './api';
+import { Link } from 'react-router-dom';
+
+class Register extends React.Component {
+    state = {
+        email: '',
+        password: '',
+        confirmPassword: ''
+    };
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    };
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        threel_api.post('/auth/register', {
+            email: this.state.email,
+            password: this.state.password
+        })
+            .then(response => {
+                console.log(`User registered`);
+                alert("User registered successfully!");
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    };
+
+
+
+
+    render() {
+        return (
+            <div className="App">
+                <h1 className='text-center'>THREEL</h1>
+                <div className="registerContainer mx-auto my-auto">
+                    <h2 className='text-center'>REGISTER</h2>
+                    <form className='form-group'>
+
+                        <input
+                            name='email'
+                            type='email'
+                            id='email'
+                            placeholder='Email'
+                            value={this.state.email}
+                            onChange={this.handleChange}
+                            required
+                        />
+                        <input
+                            name='password'
+                            id='password'
+                            type='password'
+                            placeholder='Password'
+                            value={this.state.password}
+                            onChange={this.handleChange}
+                            required
+                        />
+                        <input
+                            name='confirmPassword'
+                            id='confirm-password'
+                            type='password'
+                            placeholder='Confirm Password'
+                            value={this.state.confirmPassword}
+                            onChange={this.handleChange}
+                            required
+                        />
+
+                        <p className='mx-auto'>Already have an Account? <Link to="/login">Login Now!</Link></p>
+
+                        <div className='mx-auto'>
+                            <button type="submit" className="registerButton" onClick={this.handleSubmit}>Register</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Register;
